5d78f915a4e98193b749921cfe5208c15445c2ab
Alexei Sorokine <2picae@gmail.com>
2019-02-13 15:50:25 -0800

    Update cart qty if the same item added

--- data/index.js
+++ data/index.js
@@ -34,18 +34,35 @@ const getRandomItem = function(callback) {
 };
 
 const addItemToCart = function(user, item_id, callback) {
-  db.query(('INSERT INTO cart (user, item_id, qty) VALUES ("' + user + '",' + item_id + ',' + 1 + ')'), function(err) {
+  db.query(('SELECT id FROM cart WHERE item_id = "' + item_id + '"'), function(err, res) {
     if (err) {
-      console.log('ERROR cart', err);
-      return callback(err);
+      return console.log(err);
+    }
+    if (!res.length) {
+      db.query(('INSERT INTO cart (user, item_id, qty) VALUES ("' + user + '",' + item_id + ',' + 1 + ')'), function(err) {
+        if (err) {
+          console.log('ERROR cart', err);
+          return callback(err);
+        }
+      });
+    } else {
+      db.query(('UPDATE cart SET qty = qty + 1 WHERE item_id = ' + item_id), function(err) {
+        if (err) {
+          console.log('ERROR cart', err);
+          return callback(err);
+        }
+      });
     }
     db.query(('SELECT * FROM cart WHERE user = "' + user + '"'), function(err, res) {
       if (err) {
         console.log('ERROR cart', err);
         return callback(err);
       }
-      callback(null, res.length);
-    })
+      let quantity = res.reduce((acc, item) => {
+        return acc + item.qty;
+      }, 0);
+      callback(null, quantity);
+    });
   });
 };
 
