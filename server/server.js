7139ef4ff057a520763f59c774fad617a11bf74c
Alexei Sorokine <2picae@gmail.com>
2019-02-24 12:27:00 -0800

    Change server error handling for Docker

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const parser = require('body-parser');
 const express = require('express');
 const path = require('path');
@@ -5,12 +7,23 @@ const db = require('./../data/index.js');
 const port = 3001;
 let app = express();
 
+console.log('environment variables:');
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+});
+
 app.use(parser.json());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use(express.static(__dirname + '/../public'));
 app.use(express.static(__dirname + '/../dist'));
 app.use(express.static(__dirname + '/../data'));
@@ -25,7 +38,9 @@ app.get('/search/:str', function(req, res) {
   let str = req.params.str;
   db.search(str, function(err, items) {
     if (err) {
-      return console.error(err);
+      console.error(err);
+      res.status(404).send();
+      return;
     }
     res.send(items);
   });
@@ -33,14 +48,32 @@ app.get('/search/:str', function(req, res) {
 
 app.get('/emptycart/:user', function(req, res) {
   let user = req.params.user;
-  db.emptyCart(user);
-  res.end();
+  db.emptyCart(user, function(err) {
+    if (err) {
+      console.error(err);
+      res.status(404).send();
+      return;
+    }
+    res.end();
+  });
 });
 
 app.get('/addtocart/:user/:id', function(req, res) {
   let {user, id} = req.params;
-  db.addItemToCart(user, id, function(err, qty) {
-    res.send('' + qty);
+  db.getItemById(id, function(err, data) {
+    if (err) {
+      console.error(err);
+      res.status(404).send();
+      return;
+    }
+    db.addItemToCart(user, id, function(err, qty) {
+      if (err) {
+        console.error(err);
+        res.status(404).send();
+        return;
+      }
+      res.send({item: data, qty: qty});
+    });
   });
 });
 
@@ -48,22 +81,28 @@ app.get('/addrandomtocart/:user', function(req, res) {
   let user = req.params.user;
   db.getRandomItem(function(err, data) {
     if (err) {
-      return console.log(err);
+      console.error(err);
+      res.status(404).send();
+      return;
     }
     db.addItemToCart(user, data.id, function(err, qty) {
       if (err) {
-        return console.log(err);
+        console.error(err);
+        res.status(404).send();
+        return;
       }
       res.send({item: data, qty: qty});
-    })
-  })
+    });
+  });
 });
 
 app.get('/signin/:user', function(req, res) {
   let user = req.params.user;
   db.transferCart(user, function(err, qty) {
     if (err) {
-      return console.log(err);
+      console.error(err);
+      res.status(404).send();
+      return;
     }
     res.send(qty);
   });
@@ -73,10 +112,23 @@ app.get('/get3randomitems', function(req, res) {
   let itemName = req.params.itemName;
   db.get3RandomItems(function(err, data) {
     if (err) {
-      return console.log(err);
+      console.error(err);
+      res.status(404).send();
+      return;
     }
     res.send(data)
   });
 });
 
-app.listen(port, () => console.log(`sever is listening on port ${port}!`));
+app.get('/:productID', (request, response) => {
+  let productID = request.params.productID;
+
+  if (isNaN(Number(productID))) {
+    console.log('Check product ID number!');
+    response.status(404).send();
+  } else {
+    response.redirect(`/index.html?productID=${productID}`);
+  }
+ });
+
+app.listen(port, () => console.log(`sever is listening on : ${process.env.MY_URL} :)`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,47 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+dotenv.config();
+let MY_URL = process.env.MY_URL;
+if (MY_URL === 'http://localhost') {
+  MY_URL = 'http://localhost:3001';
+}
 
-module.exports = {
-  mode: 'development',
-  entry:  path.resolve(__dirname, 'client', 'index.jsx'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /(.js$|.jsx$)/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = env => {
+  console.log('Building with url: ', MY_URL)
+  return {
+    mode: 'development',
+    entry:  path.resolve(__dirname, 'client', 'index.jsx'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /(.js$|.jsx$)/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    node: {
+      fs: 'empty'
+    },
+    plugins: [
+      new webpack.DefinePlugin({ 'process.env.MY_URL': JSON.stringify(MY_URL) })
     ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  }
+  };
 };
